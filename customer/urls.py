b3b4263b23d1fb5d1486a5c0691f5b1fd466a343
Paula Lobo Ferreira <paulakaferreira@gmail.com>
2023-06-10 10:05:13 +0200

    Create registration page

---
+++ customer/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'customer'
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('registration-success/', views.registration_success, name='registration_success'),
+    # Other URL patterns for the "customer" app can be added here
+]
