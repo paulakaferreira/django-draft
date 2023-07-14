0592c32b9273a1330c71b45e983bb5a8219930b0
Paula Lobo Ferreira <paulakaferreira@gmail.com>
2023-07-14 21:04:17 +0200

    Add cart_view

--- cart/urls.py
+++ cart/urls.py
@@ -5,4 +5,5 @@ app_name = 'cart'
 
 urlpatterns = [
     path('', views.cart_view, name='cart_view'),
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Cart, Product
 
 @login_required(login_url='/customer/login')
 def cart_view(request):
-    return render(request, 'cart.html')
+    customer = request.user.customerprofile
+    cart_items = Cart.objects.filter(customer=customer)
+    context = {'cart_items': cart_items}
+    return render(request, 'cart_view.html', context)
+
+
+@login_required(login_url='/customer/login')
+def add_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    customer = request.user.customerprofile
+    cart_item, created = Cart.objects.get_or_create(product=product, customer=customer)
+    if not created:
+        cart_item.number += 1
+        cart_item.save()
+    return redirect('cart:cart_view')
