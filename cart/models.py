85d41fd36434bb88f72e5eb8c600b147cf42a90d
Paula Lobo Ferreira <paulakaferreira@gmail.com>
2023-07-14 18:22:15 +0200

    Remove unused code on Cart app

--- cart/models.py
+++ cart/models.py
@@ -3,19 +3,6 @@ from catalog.models import Product
 from customer.models import CustomerProfile
 
 
-'''class Cart(models.Model):
-    products = models.ManyToManyField(Product, through="CartProduct")
-    total = models.PositiveIntegerField(default=0)
-
-    #def __str__(self):
-     #   return f"{self.customer}'s cart"
-    
-    def is_empty(self):
-        if self.products.all():
-            return False
-        else:
-            return True '''
-
 class Cart(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     customer = models.ForeignKey(CustomerProfile, on_delete=models.CASCADE, related_name='cart')
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import get_object_or_404, render
-from cart.models import Cart
-from customer.models import CustomerProfile
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/customer/login')
