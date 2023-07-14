b7081b54a94482e1671296a358f2b351a75dbb12
Paula Lobo Ferreira <paulakaferreira@gmail.com>
2023-07-14 21:15:26 +0200

    Add 'subtotal' to cart

--- cart/models.py
+++ cart/models.py
@@ -7,7 +7,9 @@ class Cart(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     customer = models.ForeignKey(CustomerProfile, on_delete=models.CASCADE, related_name='cart')
     number = models.PositiveIntegerField(default=1)
-    subtotal = models.PositiveIntegerField(default=0)
+
+    def subtotal(self):
+        return self.number * self.product.price
 
     def __str__(self):
         return f"{self.product} / {self.customer}"
