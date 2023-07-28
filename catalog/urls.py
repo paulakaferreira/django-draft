4d9e5344132e662ef17d38e4620b42a4eedc6e93
gabrielmenevis <128372786+gabrielmenevis@users.noreply.github.com>
2023-07-28 21:28:47 +0200

    Add review feature
    - Create ReviewForm
    - Add form to product.html
    - Create add_review view and map it in urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,5 +6,6 @@ app_name = 'catalog'
 urlpatterns = [
     path('product/<slug:slug>/', views.product_view, name='product_view'),
     path('category/<slug:slug>/', views.category_view, name='category_view'),
+    path('add_review/<slug:slug>/', views.add_review, name='add_review'),
     # Other URL patterns for the "catalog" app can be added here
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from catalog.models import Product, Category
+from .forms import ReviewForm
 
 # Create your views here.
 
 def product_view(request, slug):
     product = get_object_or_404(Product, slug=slug)
-    context = {'product': product}
+    review_form = ReviewForm()
+    context = {
+        'product': product,
+        'review_form': review_form,
+        }
     return render(request, 'product.html', context)
 
 def category_view(request, slug):
     category = get_object_or_404(Category, slug=slug)
     context = {'category': category}
     return render(request, 'category.html', context)
+
+def add_review(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    if request.method == 'POST':
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.customer = request.user.customerprofile
+            review.product = product
+            review.save()
+    return redirect('catalog:product_view', slug=slug)
