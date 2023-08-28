562b680bbce9a738ff83dd5bba60c5c805decb1a
gabrielmenevis <128372786+gabrielmenevis@users.noreply.github.com>
2023-08-28 19:21:41 +0200

    More docstrings and unused code deletion

--- cart/forms.py
+++ cart/forms.py
@@ -1,14 +1,3 @@
 from django import forms
 from . import models
 
-
-"""class CartForm(forms.ModelForm):
-    class Meta:
-        model = models.Cart
-        exclude = ['total']
-
-class CartProductForm(forms.ModelForm):
-    class Meta:
-        model = models.CartProduct
-        exclude = ['subtotal']
-"""
--- homepage/views.py
+++ homepage/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from catalog.models import Product
 
 def home_page_view(request):
+    """Renders homepage view with all products data"""
     products = Product.objects.all()  # Retrieve all products from the database
     return render(request, 'home.html', {'products': products})
 
